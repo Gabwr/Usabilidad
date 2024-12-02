20118820c481586b67e8de6285c59ddf6dc1e181
Gabriel López <gamalop0104@gmail.com>
2024-12-02 15:53:32 -0500

    1.0.0

---
+++ javascript/modal.js
@@ -0,0 +1,9 @@
+ 
+    function openModal() {
+      document.getElementById('modal').style.display = 'block';
+      document.getElementById('overlay').style.display = 'block';
+    }
+    function closeModal() {
+      document.getElementById('modal').style.display = 'none';
+      document.getElementById('overlay').style.display = 'none';
+    }
