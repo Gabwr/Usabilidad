ec0e4734d3290dca668b2fa5599f25be9be1a294
unknown <matemedranda15@gmail.com>
2024-12-04 16:41:28 -0500

    1.2.0

--- javascript/modal.js
+++ javascript/modal.js
@@ -7,3 +7,23 @@ function closeModal() {
   document.getElementById('modal').style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
 }
+
+var botonMic = document.getElementById("mic");
+var salida = document.getElementById("salida");
+
+let recognition;
+recognition = new webkitSpeechRecognition();
+recognition.lang = "es-ES";
+
+recognition.onresult = function (event) {
+	const result = event.results[0][0].transcript;
+	const notaDiv = document.createElement("salida");
+	
+	notaDiv.textContent = result;
+	salida.append(notaDiv);
+};
+
+
+botonMic.addEventListener("click", function(){
+	recognition.start();
+});
