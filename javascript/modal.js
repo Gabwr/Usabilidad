8506072e8fdd3a92a6c33aec9e613aab0814eb7f
Gabriel López <gamalop0104@gmail.com>
2024-12-04 21:04:39 -0500

    1.5.0

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,6 +1,9 @@
 function openModal() {
-  document.getElementById('modal').style.display = 'block';
-  document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+
+    const textoSalida = document.getElementById('salida').innerHTML;//muestra el texto de salida en el overlay
+    document.getElementById('textoModal').innerHTML = textoSalida;
 }
 
 function closeModal() {
@@ -8,6 +11,15 @@ function closeModal() {
   document.getElementById('overlay').style.display = 'none';
 }
 
+    function changeTextArial() {
+            const textoModal = document.getElementById('textoModal');
+      
+      textoModal.style.fontFamily = textoModal.style.fontFamily === 'Arial, sans-serif'
+        ? "'Comic Sans MS', cursive"
+        : "Arial, sans-serif";
+    }
+
+//Funciones de toma de microfono
 var botonMic = document.getElementById("mic");
 var salida = document.getElementById("salida");
 
@@ -15,13 +27,24 @@ let recognition;
 recognition = new webkitSpeechRecognition();
 recognition.lang = "es-ES";
 
+const salidas = document.getElementById('salida');
+salidas.innerHTML = 'Tu texto esta aqui';
+
+//funcion mostar en el div texto incial
+function mostrarTexto() {
+        const inputTexto = document.getElementById('texto').value; 
+        const salida = document.getElementById('salida');
+        salida.innerHTML = inputTexto; 
+    }
+
 recognition.onresult = function (event) {
+	salida.innerHTML = ""; 
 	const result = event.results[0][0].transcript;
 	const notaDiv = document.createElement("salida");
-	
 	notaDiv.textContent = result;
-	salida.append(notaDiv);
+	salida.append(notaDivc);
 };
+
 
 
 botonMic.addEventListener("click", function(){
